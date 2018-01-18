f83cf1228ce04e1e2a7995271537275f96712e86
BereRougerio <bere.rougerio@gmail.com>
2018-01-18 10:08:28 -0600

    agregando funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,48 +1,56 @@
 
   //Traer elementos del HTML
-  
-  
-  var loadPage = function () {
-    
+  //
+  var $nameInput = $("#name-input");  // nombre
+  var $phoneInput = $("#phone-imput");  // telefono
+
+  function loadPage () {
+    $(".modal").modal();  // se mandan llamar a la clase modal, que despliega las cajas contenedoras
+    $nameInput.keyup(validateContact); // validar que el usuario ingrese datos. Que este tecleando un nombre
+    $phoneInput.keyup(validateContact); // validar que el usuario ingrese datos. Que este tecleando números
   };
 
   var paintContactsInHTML = function(contact) {
-   
+
     /* Crear elementos con DOM html al publicar contacto */
-     
-    
+
+
     //Asignando atributos y/o eventos
-    
+
 
     /* Asignando valores a los elementos*/
-    
+
 
     //Agregamos lo que creamos con el DOM a un elemento existente del HTML
-    
+
   };
 
   var addContact = function (e) {
     e.preventDefault();
     //Las siguientes lineas toman el valor que el usuario agrega en los inputs y los guardan en variables
-    
+
 
     //Con las variables separadas que obtuvimos creamos una estructura de datos unica, un objeto por cada contacto
-    
+
 
     /*Agregamos el contacto a nuestra data (arreglo declarado) para poderla filtrar y eliminar posteriormente*/
-    
+
     //La siguiente funcion se encarga de pintar los contactos en el html
-    
+
     /* Limpiando valores de formulario*/
-    
+
   };
-
-  
-
 
   var validateContact = function () {
-    //En esta funcion tenemos que validar que el usuario ingrese datos y no valores vacios
+  //En esta funcion tenemos que validar que el usuario ingrese datos y no valores vacios
+    var $containerAddContact = $("#add-contact")
+    if($(this).val().trim().length > 0){   // trim elimina los espacios en blanco en ambos extremos del string.
+      $containerAddContact.removeAttr("disabled");
+    } else {
+      $containerAddContact.Attr("disabled", true);
+    }
   };
+
 
 
   var filterContacts = function() {
